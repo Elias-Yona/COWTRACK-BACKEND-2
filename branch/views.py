4a3b2a795e2ea039757f1e8a493a4a41e7fc6f3b
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2023-11-03 21:55:21 +0300

    chore: Restrict access to only superusers

--- branch/views.py
+++ branch/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth import get_user_model
 from .serializers import CustomerSerializer, UserSerializer
 from .models import Customer
+from .permissions import IsSuperUser
 
 
 User = get_user_model()
@@ -11,8 +12,10 @@ class CustomerViewSet(ModelViewSet):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all().select_related(
         'user').order_by('-user__date_joined')
+    permission_classes = (IsSuperUser,)
 
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all().order_by('-date_joined')
+    permission_classes = (IsSuperUser,)
