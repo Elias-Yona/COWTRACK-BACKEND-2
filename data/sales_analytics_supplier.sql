04b4cc34945704b268316744225c9a2987f2b71a
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2023-11-05 00:14:18 +0300

    chore: Add supplier test data

---
+++ data/sales_analytics_supplier.sql
@@ -0,0 +1,64 @@
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (1, '(109) 4762472', '745-90-8232', '(271) 1984054', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
+
+Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', 576);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (2, '(353) 7212758', '160-39-7257', '(883) 6257510', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
+
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 577);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (3, '(786) 6573763', '888-64-2422', '(773) 5755680', 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
+
+In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 578);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (4, '(881) 5526679', '695-89-4884', '(575) 7742932', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 579);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (5, '(344) 3150481', '526-57-0734', '(850) 3717745', 'In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 580);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (6, '(357) 2888357', '459-90-6815', '(409) 6752799', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 581);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (7, '(184) 7233671', '823-13-2417', '(890) 3888462', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 582);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (8, '(546) 9945815', '171-38-7097', '(425) 5950805', 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 583);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (9, '(876) 8452335', '440-76-6291', '(279) 5331994', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.
+
+Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 584);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (10, '(148) 5722453', '631-29-7304', '(452) 4201674', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 585);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (11, '(713) 5290046', '416-74-8360', '(113) 1416840', 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.
+
+Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 586);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (12, '(935) 2701382', '389-10-8408', '(856) 2210938', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 587);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (13, '(803) 5314257', '554-70-1418', '(233) 5213370', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 588);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (14, '(849) 9301587', '453-19-5550', '(684) 5423986', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 589);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (15, '(888) 2423983', '810-01-3548', '(682) 1284341', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
+
+Sed ante. Vivamus tortor. Duis mattis egestas metus.
+
+Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 590);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (16, '(663) 3191286', '481-67-6683', '(435) 5014776', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
+
+Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.', 591);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (17, '(972) 6374736', '599-05-7711', '(122) 7914023', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
+
+Sed ante. Vivamus tortor. Duis mattis egestas metus.
+
+Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 592);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (18, '(637) 3384240', '486-62-6848', '(403) 7834105', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', 593);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (19, '(848) 9219390', '732-37-1257', '(533) 3398590', 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 594);
+insert into sales_analytics_supplier (supplier_id, phone_number, kra_pin, contact_person, notes, user_id) values (20, '(656) 3226216', '610-27-9620', '(660) 9129080', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
+
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
+
+Sed ante. Vivamus tortor. Duis mattis egestas metus.', 595);
