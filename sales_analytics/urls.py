f5f946733e034c10c1439ec3ecd9f4342574b3a2
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2023-11-05 00:02:18 +0300

    chore: Add supplier URL

--- sales_analytics/urls.py
+++ sales_analytics/urls.py
@@ -9,6 +9,7 @@ router.register('all-users', views.UserViewSet, basename='users-list')
 router.register('salespersons', views.SalesPersonViewSet, basename='salespersons-list')
 router.register('branches', views.BranchViewSet, basename='branches-list')
 router.register('managers', views.ManagerViewSet, basename='managers-list')
+router.register('suppliers', views.SupplierViewSet, basename='suppliers-list')
 
 salesperson_router = routers.NestedSimpleRouter(router, r'salespersons', lookup='salesperson')
 salesperson_router.register(r'branches', views.SalesPersonBranchViewSet, basename='salesperson-branch')
