5539ea7bcb09c13900ad56143560561b4534a8d8
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2023-11-04 22:24:45 +0300

    chore: Add managers URL

--- sales_analytics/urls.py
+++ sales_analytics/urls.py
@@ -8,6 +8,7 @@ router.register('customers', views.CustomerViewSet, basename='customers-list')
 router.register('all-users', views.UserViewSet, basename='users-list')
 router.register('salespersons', views.SalesPersonViewSet, basename='salespersons-list')
 router.register('branches', views.BranchViewSet, basename='branches-list')
+router.register('managers', views.ManagerViewSet, basename='managers-list')
 
 salesperson_router = routers.NestedSimpleRouter(router, r'salespersons', lookup='salesperson')
 salesperson_router.register(r'branches', views.SalesPersonBranchViewSet, basename='salesperson-branch')
