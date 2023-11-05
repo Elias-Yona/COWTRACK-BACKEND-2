e31e6748d6eba4d7f773da3d1895f3c9226e2b7f
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2023-11-05 16:20:05 +0300

    chore: Add cart url

--- sales_analytics/urls.py
+++ sales_analytics/urls.py
@@ -13,9 +13,13 @@ router.register('suppliers', views.SupplierViewSet, basename='suppliers-list')
 router.register('product-categories', views.ProductCategoryViewSet, basename='productcategories-list')
 router.register('products', views.ProductViewSet, basename='products-list')
 router.register('payment-methods', views.PaymentMethodViewSet, basename='paymentmethods-list')
+router.register('cart', views.CartViewSet, basename='cart-list')
+
 
 salesperson_router = routers.NestedSimpleRouter(router, r'salespersons', lookup='salesperson')
 salesperson_router.register(r'branches', views.SalesPersonBranchViewSet, basename='salesperson-branch')
 
+customers_router = routers.NestedSimpleRouter(router, r'customers', lookup='customer')
+customers_router.register(r'cart', views.SalesPersonBranchViewSet, basename='salesperson-branch')
 
 urlpatterns = router.urls + salesperson_router.urls
