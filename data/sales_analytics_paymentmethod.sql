78e1d50e2439438ce339d9969acc4b2a3edae7ea
IconElias254 <67762511+IconElias254@users.noreply.github.com>
2023-11-05 15:50:13 +0300

    chore: Add paymentmethod serializer

---
+++ data/sales_analytics_paymentmethod.sql
@@ -0,0 +1,2 @@
+insert into sales_analytics_paymentmethod (payment_method_id, method_name) values (1, 'M-PESA');
+insert into sales_analytics_paymentmethod (payment_method_id, method_name) values (2, 'M-PESA');
